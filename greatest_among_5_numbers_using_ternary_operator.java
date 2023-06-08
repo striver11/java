5368d18f92517979975c161282988752ee676ef7
striver11 <83543044+striver11@users.noreply.github.com>
2023-06-08 22:53:48 +0530

    Create greatest_among_5_numbers_using_ternary_operator.java

---
+++ greatest_among_5_numbers_using_ternary_operator.java
@@ -0,0 +1,19 @@
+package jspiders_anupama_mam;
+import java.util.Scanner;
+public class Prime {
+	
+
+public static void main(String[] args) {
+
+	int a=5;
+	int b=40;
+	int c=30;
+	int d=20;
+	int e=100;
+	System.out.println(a>b&&a>c&&a>d&&a>e? a+"is greater":b>c&&b>d&&b>e? b+"is greater":c>d&&c>e? c+"is greater ":d>e?d+"is greater":e+"is greater");
+			
+}
+	
+
+
+}
