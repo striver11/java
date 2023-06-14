ae6bf064be7eb16da824a21a21320383a82e9780
striver11 <kurubapraveenkumar1@gmail.com>
2023-06-14 22:19:38 +0530

    Remove all from git

---
+++ patterns/Left_triangel.java
@@ -0,0 +1,13 @@
+package patterns;
+
+public class Left_triangel {
+    public static void main(String[] args){
+        for(char ch='a';ch<='z';ch++)
+        {
+            int asci=ch;
+            System.out.println(ch+" "+asci);
+            System.out.println("this is called the data hidden property");
+        }
+    }
+    
+}
