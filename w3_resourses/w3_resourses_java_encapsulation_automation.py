2d64a01f680908212678763a619cd0b2ff3a0cc0
striver11 <83543044+striver11@users.noreply.github.com>
2023-06-14 17:18:28 +0530

    Create w3_resourses_java_encapsulation_automation.py

---
+++ w3_resourses/w3_resourses_java_encapsulation_automation.py
@@ -0,0 +1,42 @@
+import time
+import os
+import webbrowser
+j=0
+i=1
+l=  [ 'https://www.w3resource.com/java-exercises/encapsulation/java-encapsulation-exercise-1.php', 'https://www.w3resource.com/java-exercises/encapsulation/java-encapsulation-exercise-2.php', 'https://www.w3resource.com/java-exercises/encapsulation/java-encapsulation-exercise-3.php', 'https://www.w3resource.com/java-exercises/encapsulation/java-encapsulation-exercise-4.php', 'https://www.w3resource.com/java-exercises/encapsulation/java-encapsulation-exercise-5.php', 'https://www.w3resource.com/java-exercises/encapsulation/java-encapsulation-exercise-6.php', 'https://www.w3resource.com/java-exercises/encapsulation/java-encapsulation-exercise-7.php']
+
+
+
+
+
+
+
+
+
+# p=[]
+# for i in k:
+#     if i not in p:
+#         p.append(i)
+
+# l=[]
+# for i in p:
+#     if  i not in l:
+#         l.append(i)
+# print(l)
+while j <len(l):
+    while i==1:
+        url=l[j]
+        def countdown(time_sec):
+            global j
+            while time_sec:
+                mins, secs = divmod(time_sec, 60)
+                timeformat = '{:02d}:{:02d}'.format(mins, secs)
+                print(timeformat, end='\r')
+                time.sleep(1)
+                time_sec -= 1
+            s=1
+            webbrowser.open(url,new=s)
+            print(j)
+            j=j+1
+            return j
+        t=countdown(6)
