9a5ce8a5b3a5fe363783f3812ac79156d3edf67e
striver11 <83543044+striver11@users.noreply.github.com>
2023-06-20 22:52:34 +0530

    Create return_is_not_mandatory_if_void_is_used.java

---
+++ java_class/methods/types_to_declare_method_in_java/pass_no_arguments_return_no_value_or_without_parameter_without_return_type/return_is_not_mandatory_if_void_is_used.java
@@ -0,0 +1,24 @@
+public class return_is_not_mandatory_if_void_is_used {
+    public static void main(String[] args) {
+        hello();
+    }
+    public static void hello()
+    {
+        System.out.println("Hello");
+        return;
+    }
+    
+}
+
+
+/*
+Note:
+ return is not mandatory for void declared fucntions because the compiler will implicitly we can also add return to the psvm()
+ 
+ */
+
+
+ /*
+  F:\praveen_java1\patterns>java return_is_not_mandatory_if_void_is_used
+Hello
+  */
