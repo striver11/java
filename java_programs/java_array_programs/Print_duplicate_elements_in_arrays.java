9a63e7deb25d75a2114e8b25f107ab83c4403ea1
striver11 <83543044+striver11@users.noreply.github.com>
2023-08-25 15:25:22 +0530

    Create Print_duplicate_elements_in_arrays.java

---
+++ java_programs/java_array_programs/Print_duplicate_elements_in_arrays.java
@@ -0,0 +1,35 @@
+public class duplicate {
+    public static void main(String ... duplicates) {
+        int a[]={1,2,3,4,4,1,2,7,6};
+        int b[]=new int[a.length];
+        for(int i=0;i<=a.length-1;i++)
+        {
+            int count=1;
+            for(int j=i+1;j<=a.length-1;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    b[j]=-1;
+                    count++;
+                }
+            }
+            if(b[i]!=-1)
+            {
+                b[i]=count;
+            }
+        }
+
+        for(int i=0;i<a.length;i++)
+        {
+            if(b[i]==-1)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+    }
+    
+}
+
+
+//output:
+//4 1 2 
