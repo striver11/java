39e7cf2f5ccf59ee6a4ff28ee259b51682bd9ab9
striver11 <83543044+striver11@users.noreply.github.com>
2023-09-13 23:08:54 +0530

    Create Set_Get_ThreadPriority.java

---
+++ java_MultiThreading/Set_Get_ThreadPriority.java
@@ -0,0 +1,46 @@
+package MUltiThreading;
+
+public class Set_Get_ThreadPriority {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Runnable r1=()->
+		{
+			for(int i=0;i<=10;i++)
+			{
+				System.out.print(i);
+			}
+		};
+		
+		Thread t1=new Thread(r1);
+		t1.start();
+		
+		
+	System.out.println("default priority of main Thread  "+Thread.currentThread().getPriority());
+	System.out.println("Default priority of t1 thread  "+t1.getPriority()); 
+	System.out.println();
+//	System.out.println("Name of t1 thread   :"+t1.getName());
+//	System.out.println("Name of the main tread   :"+Thread.currentThread().getName());
+	System.out.println();
+	Thread.currentThread().setPriority(7);//highest priority
+	t1.setPriority(3);
+	
+	System.out.println("priority of main Thread after changing  "+Thread.currentThread().getPriority());
+	System.out.println("priority of t1 thread after changing "+t1.getPriority()); 
+	
+	
+	System.out.println();
+	
+	for(char i='a';i<='g';i++)
+	{
+		System.out.print(i);
+	}
+	
+	
+	
+	
+
+	}
+
+}
