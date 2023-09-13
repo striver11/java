26fc0f84528d5887f764b1e9019d5454ceb949ad
striver11 <83543044+striver11@users.noreply.github.com>
2023-09-13 23:15:33 +0530

    Update set_get_ThreadName.java

--- java_MultiThreading/set_get_ThreadName.java
+++ java_MultiThreading/set_get_ThreadName.java
@@ -31,11 +31,13 @@ public class set_get_ThreadName {
 
 
 
-// outPut:
-// Name of t1 Thread  : Thread-0
-// Name of main Thread : main
-// Name of t1 Thread after changed : t1 Thread
-// Name of main Thread after chnaged : Main thread Changed
-// Name of t2 Thread after changed : t2 Thread
-// Exception in thread "Main thread Changed" java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 3
-// 	at MultiThreading/MUltiThreading.set_get_ThreadName.main(set_get_ThreadName.java:25)
+//
+//output:
+//	Name of t1 Thread  : Thread-0
+//	Name of main Thread : main
+//	Name of t1 Thread after changed : t1 Thread
+//	Name of main Thread after chnaged : Main thread Changed
+//	Name of t2 Thread after changed : t2 Thread
+//	Exception in thread "Main thread Changed" java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 3
+//		at MultiThreading/MUltiThreading.set_get_ThreadName.main(set_get_ThreadName.java:25)
+
