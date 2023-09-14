6778a8d7ab74344c701e3e4b83961462eadcf0b6
striver11 <83543044+striver11@users.noreply.github.com>
2023-09-14 14:41:34 +0530

    Create One_synchronized_static_one_nonSynchronized_nonStatic_menthods.java

---
+++ java_MultiThreading/One_synchronized_static_one_nonSynchronized_nonStatic_menthods/One_synchronized_static_one_nonSynchronized_nonStatic_menthods.java
@@ -0,0 +1,31 @@
+package MUltiThreading;
+
+
+//produces irregular output because the m1() is Sychronized static method so it requires CLASS LEVEL LOCK  to work
+//m2() is non-static method so it requires no lock so it might come in middle and exicue
+
+
+//output:	THE OUTPUR IS IRREGULAR
+//	a12bc3d45e6fg78h9ij10k11l12m13n14o1516p17qr1819st20uvwxyz
+
+public class One_synchronized_static_one_nonSynchronized_nonStatic_menthods {
+	
+	public static synchronized void m1() throws InterruptedException
+	{
+		for(int i=1;i<=20;i++)
+		{
+			System.out.print(i);
+			Thread.sleep(20);
+		}
+	}
+	
+	public void m2() throws InterruptedException
+	{
+		for(char c='a';c<='z';c++)
+		{
+			System.err.print(c);
+			Thread.sleep(20);
+		}
+	}
+
+}
