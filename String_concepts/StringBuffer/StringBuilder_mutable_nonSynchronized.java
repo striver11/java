54aaf784376ba0988bbd329252193180c553285a
striver11 <83543044+striver11@users.noreply.github.com>
2023-09-23 23:58:40 +0530

    Create StringBuilder_mutable_nonSynchronized.java

---
+++ String_concepts/StringBuffer/StringBuilder_mutable_nonSynchronized.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class StringBuilder_mutable_nonSynchronized {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder b=new StringBuilder("hello");
+		b.append(" dabba");
+		
+		System.out.println(b);
+
+	}
+
+}
+
+//output:
+//hello dabba
+
