588508946534ea7786d3166e6ce1eca4d102aecc
striver11 <83543044+striver11@users.noreply.github.com>
2023-09-23 23:54:14 +0530

    Create StringBuffer_Mutable_Synchronized.java

---
+++ String_concepts/StringBuffer/StringBuffer_Mutable_Synchronized.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class StringBuffer_Mutable_Synchronized {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb=new StringBuffer("hello");
+		sb.append("  dabba");
+		System.out.println(sb);
+
+	}
+
+}
+
+
+// output:
+// hello  dabba
