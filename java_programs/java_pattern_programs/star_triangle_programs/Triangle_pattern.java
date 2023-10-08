82a38e8b02c0327070751a3c1b9a1987a10eea5d
striver11 <83543044+striver11@users.noreply.github.com>
2023-10-09 04:18:16 +0530

    Create Triangle_pattern.java

---
+++ java_programs/java_pattern_programs/star_triangle_programs/Triangle_pattern.java
@@ -0,0 +1,39 @@
+package star_triangle_patterns;
+
+//8
+//			        * 
+//		        * * * 
+//          * * * * * 
+//        * * * * * * * 
+//      * * * * * * * * * 
+//    * * * * * * * * * * * 
+//  * * * * * * * * * * * * * 
+//* * * * * * * * * * * * * * *
+
+
+
+import java.util.*;
+public class Triangle_pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int m=1;m<=n-i;m++)
+			{
+				System.out.print(" "+" ");
+			}
+			for(int j=1;j<=2*i-1;j++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
